330662b8fc4a75aeea8dceb19753aca1d37fe54d
PiyushXCoder <piyush.raj.kit@gmail.com>
2025-03-09 12:44:31 +0530

    refactor: Create new repository with elixir and rustler project

---
+++ lib/cnft_elixir.ex
@@ -0,0 +1,18 @@
+defmodule CnftElixir do
+  @moduledoc """
+  Documentation for `CnftElixir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CnftElixir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/cnft_elixir_test.exs
@@ -0,0 +1,8 @@
+defmodule CnftElixirTest do
+  use ExUnit.Case
+  doctest CnftElixir
+
+  test "greets the world" do
+    assert CnftElixir.hello() == :world
+  end
+end
