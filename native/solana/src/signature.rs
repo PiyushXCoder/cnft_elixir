0ed2e2043d50830d2a2d376242a71c52758bc9f8
PiyushXCoder <piyush.raj.kit@gmail.com>
2025-03-09 13:53:49 +0530

    refactor: Add functions to generate keypairs

--- native/solana/src/signature.rs
+++ native/solana/src/signature.rs
@@ -1,5 +1,5 @@
-use rustler::{resource_impl, Resource};
-use solana_sdk::signature::Keypair;
+use rustler::{resource_impl, Resource, ResourceArc};
+use solana_sdk::{signature::Keypair, signer::EncodableKey};
 
 pub struct KeypairWrapper {
     pub keypair: Keypair,
@@ -12,4 +12,16 @@ impl From<Keypair> for KeypairWrapper {
     fn from(value: Keypair) -> Self {
         Self { keypair: value }
     }
+}
+
+#[rustler::nif]
+fn new_keypair() -> ResourceArc<KeypairWrapper> {
+    ResourceArc::new(KeypairWrapper::from(Keypair::new()))
+}
+
+#[rustler::nif]
+fn keypair_read_from_file(file: String) -> Result<ResourceArc<KeypairWrapper>, String> {
+    Ok(ResourceArc::new(KeypairWrapper::from(
+        Keypair::read_from_file(file).map_err(|e| e.to_string())?,
+    )))
 }
