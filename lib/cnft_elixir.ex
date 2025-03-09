321b41e93acfff5cf323560aca2cc8bedf955625
PiyushXCoder <piyush.raj.kit@gmail.com>
2025-03-09 15:43:40 +0530

    feat: Add all the basic functions in elixir

--- lib/cnft_elixir.ex
+++ lib/cnft_elixir.ex
@@ -1,18 +1,32 @@
 defmodule CnftElixir do
-  @moduledoc """
-  Documentation for `CnftElixir`.
-  """
+end
 
-  @doc """
-  Hello world.
+defmodule Solana do
+  use Rustler, otp_app: :cnft_elixir, crate: :solana
+  defp error, do: :erlang.nif_error(:nif_not_found)
 
-  ## Examples
+  # Client
+  def new_rpc_client(_url), do: error()
+  def send_and_confirm_transaction(_client, _transaction), do: error()
+  def get_minimum_balance_for_rent_exemption(_client, _size), do: error()
+  def get_latest_blockhash(_client), do: error()
 
-      iex> CnftElixir.hello()
-      :world
+  # Pubkey
+  def keypair_to_public_key(_keypair), do: error()
 
-  """
-  def hello do
-    :world
-  end
+  # Keypair
+  def new_keypair(), do: error()
+  def keypair_read_from_file(_file), do: error()
+
+  # Transaction
+  def new_signed_transaction_with_payer(
+        _instructions,
+        _payer,
+        _signing_keypair,
+        _latest_blockhash
+      ),
+      do: error()
+
+  # System Program
+  def create_account_instruction(_from_pubkey, _to_pubkey, _lamports, _space, _owner), do: error()
 end
